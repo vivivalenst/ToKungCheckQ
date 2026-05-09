26cfc065c3a11ab4c0994315c93291e0fbbddeb6
vivivalenst <vivivalenst@gmail.com>
2026-05-09 22:56:47 +0700

    Initial commit

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // ══════════════════════════════════════════════════════════════
 const SUPABASE_URL      = 'https://mybytobwciyvyevfmozb.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15Ynl0b2J3Y2l5dnlldmZtb3piIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0NzQ4ODYsImV4cCI6MjA5MTA1MDg4Nn0.HhReM3O19kUMMWGtSyMuShNu4KjEzO-SRJlkbZIzyCc';
-const SHOP_ID           = '5eefe02e-ac63-49fb-bc45-f0a2d01b0f6c'; // UUID ของร้านนี้ — สร้างได้ที่ uuidgenerator.net
+const SHOP_ID           = '5db86fc7-0cc8-4e91-b5de-c92debd7b1f2'; // UUID ของร้านนี้ — สร้างได้ที่ uuidgenerator.net
 const APPS_SCRIPT_URL   = 'https://script.google.com/macros/s/AKfycbyKk0CRLbY7N7Qiim-YLR9wAd55rOEJg6QxSp0sjaYkkHcWhpFZdkLvu01Arv5yRKQPAg/exec';
